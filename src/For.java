bcbf994cfbd09e0dd871baaa402fe0970d8a2a03
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2020-12-28 15:56:33 -0300

    EstruturaFOR

---
+++ src/For.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class For {
+    public static void main(String[] args){
+
+        Scanner ale = new Scanner(System.in);
+
+        System.out.printf("Digite a quantidade de somas: ");
+        int z = ale.nextInt();
+        int soma = 0;
+
+        for (int x = 0; x<z; x++){
+            System.out.printf("Digite o valor que deseja somar: ");
+            int c = ale.nextInt();
+            soma += c;
+        }
+        System.out.printf("Sua soma é: "+soma);
+    }
+}
