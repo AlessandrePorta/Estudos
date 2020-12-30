4c3ce4b2eb021b528fdc85f5f8fc208d96339917
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2020-12-30 16:04:47 -0300

    EditandoStrings

--- src/Funções.java
+++ src/Funções.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Funções {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Scanner ale = new Scanner(System.in);
+
+        System.out.printf("Digite 3 números: ");
+        int a = ale.nextInt();
+        int b = ale.nextInt();
+        int c = ale.nextInt();
+
+        int maior = max(a, b, c);
+
+    }
+
+    public static int max(int x, int y, int z) {
+        int res;
+        if (x > y && x > z) {
+            res = x;
+        } else if (y > z) {
+            res = y;
+        } else {
+            res = z;
+        }
 
     }
 }
