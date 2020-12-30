a0565a260c77ff0ee6dfe6c77eae28a5817c4969
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2020-12-30 15:45:26 -0300

    Funções String

---
+++ src/FunçõeString.java
@@ -0,0 +1,59 @@
+import java.util.Locale;
+/*
+public class FunçõeString {
+    static class teoria {
+    public static void main(String[] args) {
+
+    Para formatar a string:
+    toLowerCase();
+    toUpperCase();
+    trim();
+
+    Para recortar a string:
+
+    substring(inicio);
+    substring(inicio, fim);
+
+    Para substituir
+
+    replace(char,char);
+    replace(string, string);
+
+    Para buscar
+
+    IndexOf;
+    LastIndexOf;
+
+    Recortar string
+
+    str.split("");
+
+    */
+
+
+    static class teste {
+        public static void main(String[] args) {
+            String teste = "     ahsudf UASHdU U Ad dhsuah       ";
+            String a = teste.toLowerCase();
+            String b = teste.toUpperCase();
+            String c = teste.trim();
+            String d = teste.substring(2);
+            String e = teste.substring(2,9);
+            String f = teste.replace('h','a');
+            String g = teste.replace('s','a');
+            int h  = teste.indexOf("ah");
+            int i = teste.lastIndexOf("a");
+
+            System.out.println("Sem edição:"+a+"\n"+ b+"\n"+ "trim (sem espaçamento)"+c+"\nsubs: "+d+"\nsubs def: " +e+"\nreplace: " +f+"\nreplace: " +g+"\nposição da letra: " +h+"\nultima posicao da letra: " +i);
+        }
+    }
+    static class teste2{
+        public static void main(String[] args){
+            String a = "Como deve ser";
+            String[] vect = a.split("");
+            String b = vect[0];
+            String c = vect[1];
+            String d = vect[2];
+        }
+    }
+}
---
+++ src/Funções.java
@@ -0,0 +1,5 @@
+public class Funções {
+    public static void main(String[] args){
+
+    }
+}
