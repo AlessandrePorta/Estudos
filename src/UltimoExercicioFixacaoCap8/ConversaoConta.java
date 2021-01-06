2908833decd6cd97a15586a00fab524cc9a9ec94
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-01-06 16:15:44 -0300

    Ultimo exercicio cap8

---
+++ src/UltimoExercicioFixacaoCap8/ConversaoConta.java
@@ -0,0 +1,8 @@
+package UltimoExercicioFixacaoCap8;
+
+public class ConversaoConta {
+
+    public static double conversor(double dolar, double dinheiro){
+        return (((dinheiro*6)/100)+dinheiro)*dolar;
+    }
+}
