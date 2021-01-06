08ea83a4d6d1f4df84cab50b5dc8526017160ddd
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-01-06 15:12:25 -0300

    atualizando

--- src/EditandoStrings.java
+++ src/EditandoStrings.java
@@ -38,14 +38,14 @@ public class EditandoStrings {
             String a = teste.toLowerCase();
             String b = teste.toUpperCase();
             String c = teste.trim();
-            String d = teste.substring(2);
+            String d = teste.substring(8);
             String e = teste.substring(2, 9);
             String f = teste.replace('h', 'a');
             String g = teste.replace('s', 'a');
             int h = teste.indexOf("ah");
             int i = teste.lastIndexOf("a");
 
-            System.out.println("Sem edição:" + a + "\n" + b + "\n" + "trim (sem espaçamento)" + c + "\nsubs: " + d + "\nsubs def: " + e + "\nreplace: " + f + "\nreplace: " + g + "\nposição da letra: " + h + "\nultima posicao da letra: " + i);
+            System.out.println("Sem edição:" + a + "\n" + b + "\n" + "trim (sem espaçamento)" + c + "\nsubs:(tira todas a letras até o número) " + d + "\nsubs def:(Mostra todas as letras até o número) " + e + "\nreplace: " + f + "\nreplace: " + g + "\nposição da letra: " + h + "\nultima posicao da letra: " + i);
         }
     }
 
@@ -56,6 +56,8 @@ public class EditandoStrings {
             String b = vect[0];
             String c = vect[1];
             String d = vect[2];
+
+            System.out.println(d);
         }
     }
 }
