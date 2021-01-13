ac9709cec4d2c7e21f50a022f587437c4840fbde
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-01-13 17:00:40 -0300

    ConstrutorAutomatico

---
+++ src/ConstrutorAutomatico/Produtos.java
@@ -0,0 +1,35 @@
+package ConstrutorAutomatico;
+
+//PROPOSTA DE MELHORIA DO OUTRO CODIGO
+
+public class Produtos {
+
+    String nome;
+    double preco;
+    int quantidade;
+    /*
+    Botão direito, "generate", "constructor", selecionar os campos.
+     */
+    public Produtos(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+
+    public double valorTotalEmEstoque() {
+        return preco * quantidade;
+    }
+
+    public void adicionandoProdutos(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removendoProdutos(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+
+    public String toString() {
+        return nome + ", $" + String.format("%.2f", preco) + ", " + quantidade + " unidades, Total: " + String.format("%.2f", valorTotalEmEstoque());
+    }
+}
