cc4c32d7b6000f331135bc4017c8fc2f7ca8e677
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-01-13 16:54:20 -0300

    GetAndSet

---
+++ src/_1/PastaDeEstudos/GetAndSet.java
@@ -0,0 +1,11 @@
+package _1.PastaDeEstudos;
+
+public class GetAndSet {
+    //serve para chamar a String privada;
+    /* private String/int getXXX(){
+            return name;
+     */
+    //Serve para alterar a String privada;
+    /* classe.setXXX("XXXXXX");
+     */
+}
