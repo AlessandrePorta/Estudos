be42bb2ebd587a5b837eaba875991d06d4696693
Alessandre Porta <ale_junior97@hotmail.com>
2021-01-15 20:23:42 -0300

    Exercicios do Pookis

---
+++ src/ExercicioPookis/Lista.java
@@ -0,0 +1,23 @@
+package ExercicioPookis;
+
+import java.util.Locale;
+
+public class Lista {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+
+        ClasseLista lista = new ClasseLista();
+
+        int[] listaDeNumeros = lista.getLista();
+
+        for ( int posicao = 0; posicao <= 2; posicao ++ ){
+            if (posicao != 2) {
+                int posicaoZero = listaDeNumeros[posicao];
+                System.out.println(posicaoZero);
+            } else{
+                System.out.println(listaDeNumeros[3]);
+            }
+        }
+    }
+}
