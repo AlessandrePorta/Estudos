d24274ba99171880d45838fc2369add6e86a6de1
Alessandre Porta <ale_junior97@hotmail.com>
2021-01-15 23:35:25 -0300

    Exercicio 7

---
+++ src/ExercicioPookis/Exercicio7/Main.java
@@ -0,0 +1,24 @@
+package ExercicioPookis.Exercicio7;
+
+import ExercicioPookis.Exercicio1.ClasseLista;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        ClasseLista lista = new ClasseLista();
+
+        int[] listaDeNumeros = lista.getListaRandomica();
+
+        //Pegar o ListaRandomica, copiar para outra lista de um jeito ordenado e printar a lista ordenada.
+
+    }
+
+}
+
+
+
+
