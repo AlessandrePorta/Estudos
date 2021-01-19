6d06101b97ef76ab26c1b2e112b5b89516f61d94
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-01-19 14:35:22 -0300

    Modificador de acesso

---
+++ src/_1PastaDeEstudos/ModificadoresDeAcesso.java
@@ -0,0 +1,13 @@
+package _1PastaDeEstudos;
+
+public class ModificadoresDeAcesso {
+    /*
+    private = membro privado, so pode ser acessado na mesma classe.
+
+    sem modificador = so pode ser acessado pelas classes de um mesmo pacote.
+
+    protected = pode ser acessado apenas no mesmo pacote.
+
+    public = pode ser acessado por qualquer classe
+     */
+}
