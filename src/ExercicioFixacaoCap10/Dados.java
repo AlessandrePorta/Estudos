047e98e4e8a898559ef942dcd6c0786a419aeaf3
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-02-02 14:52:54 -0300

    Refeito

--- src/ExercicioFixacaoCap10/Dados.java
+++ src/ExercicioFixacaoCap10/Dados.java
@@ -1,19 +1,33 @@
 package ExercicioFixacaoCap10;
 
-import java.util.Scanner;
-
 public class Dados {
-
-    Scanner scanner = new Scanner(System.in);
 
     String nome;
     String email;
-    int quartos;
-    Dados cadastro = null;
+    int quarto;
 
     public Dados(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setName(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString(){
+        return "Nome: "+ nome + ", e-mail: " + email;
+    }
 }
---
+++ src/ExercicioFixacaoCap10/Main.java
@@ -0,0 +1,35 @@
+package ExercicioFixacaoCap10;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Dados[] lista = new Dados[10];
+
+        System.out.println("Quantos quartos deseja alugar?");
+        int n = scanner.nextInt();
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Quarto - "+ i);
+            System.out.println("Digite seu nome");
+            scanner.nextLine();
+            String nome = scanner.nextLine();
+            System.out.println("Digite seu email: ");
+            String email = scanner.nextLine();
+            System.out.println("Qual quarto deseja alugar");
+            int quarto = scanner.nextInt();
+            lista[quarto] = new Dados(nome, email);
+        }
+        System.out.println("Quartos alugados");
+        for(int i = 0; i<10; i++){
+            if(lista[i] != null){
+                System.out.println(lista[i]);
+            }
+        }
+    }
+}
