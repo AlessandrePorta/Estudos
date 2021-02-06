d671477f787e15a039ebb972585037e00bb3579c
Alessandre Porta <ale_junior97@hotmail.com>
2021-02-06 02:57:07 -0300

    Exercicio Pookis não finalizado

---
+++ src/ExerciciosPookis2/Matriz.java
@@ -0,0 +1,18 @@
+package ExerciciosPookis2;
+
+public class Matriz {
+
+    private int[][] matriz =
+            {
+                    {3,8,4},
+                    {4,8,10},
+                    {1,9,1},
+                    {0,2,1},
+                    {6,10,0},
+                    {2,6,1},
+    };
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+}
