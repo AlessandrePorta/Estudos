0d4765ff6f07546ba6421b635eee36e181b07eb4
Alessandre Luis Porta Junior <ale_junior97@hotmail.com>
2021-02-25 14:56:41 -0300

    Data atual

--- src/_1PastaDeEstudos/Data.java
+++ src/_1PastaDeEstudos/Data.java
@@ -13,8 +13,15 @@ public class Data {
         Date x1 = d1.parse("25/06/2018");
         Date x2 = d2.parse("25/06/2018 15:42:07");
 
+        Date y1 = new Date();
+        Date y2 = new Date(System.currentTimeMillis());
+        Date y3 = new Date(0L);
+
         System.out.println(x1);
         System.out.println(x2);
+        System.out.println(y1);
+        System.out.println(y2);
+        System.out.println(y3);
 
     }
 }
