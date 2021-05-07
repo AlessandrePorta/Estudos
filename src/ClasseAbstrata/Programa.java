282397c16acbc6ac5b946b1ccef9e03bf3b2c825
Alessandre Porta <ale_junior97@hotmail.com>
2021-05-07 19:49:13 -0300

    ClasseAbstrata

---
+++ src/ClasseAbstrata/Programa.java
@@ -0,0 +1,17 @@
+package ClasseAbstrata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        List<Conta> lista = new ArrayList<>();
+
+        lista.add(new Poupança(1001, "Alex", 500.0, 0.01));
+        lista.add(new ContaEmpresarial(1002, "Maria", 1000.0, 400.0));
+        lista.add(new Poupança(1004, "Alex", 300.0, 0.01));
+        lista.add(new ContaEmpresarial(1005, "Ana", 500.0, 500.0));
+
+    }
+}
