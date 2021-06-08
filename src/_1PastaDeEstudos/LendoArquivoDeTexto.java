e0c3db9496a5f72f94b345e980872e0373be7945
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-08 20:21:24 -0300

    ,

--- src/_1PastaDeEstudos/LendoArquivoDeTexto.java
+++ src/_1PastaDeEstudos/LendoArquivoDeTexto.java
@@ -1,2 +1,27 @@
-package _1PastaDeEstudos;public class LendoArquivoDeTexto {
+package _1PastaDeEstudos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LendoArquivoDeTexto {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\Ale\\Desktop\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e){
+            System.out.println("Erro: "+ e.getMessage());
+        }
+        finally{
+            if(sc != null){
+                sc.close();
+            }
+        }
+    }
 }
