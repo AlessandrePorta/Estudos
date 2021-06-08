c478820498d774949bc0d323a44fbc45edae69b2
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-08 20:30:13 -0300

    FileReaderEBufferedReader

---
+++ src/_1PastaDeEstudos/FileReaderEBufferedReader.java
@@ -0,0 +1,39 @@
+package _1PastaDeEstudos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEBufferedReader {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\Ale\\Desktop\\in.txt";
+        FileReader fr = null;
+        BufferedReader bfr = null;
+
+        try {
+            fr = new FileReader(path);
+            bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        finally{
+            try {
+                if (bfr != null) {
+                    bfr.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
