05e39c2ae4097334f1c791222e5e399d2e129e62
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-09 18:20:55 -0300

    Caminho do Arquivo

---
+++ src/_1PastaDeEstudos/CaminhoDoArquivo.java
@@ -0,0 +1,25 @@
+package _1PastaDeEstudos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CaminhoDoArquivo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o caminho do arquivo: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        //Apenas o arquivo
+        System.out.println("getName: "+ path.getName());
+        //Apenas o caminho
+        System.out.println("getParent: "+path.getParent());
+        //Caminho com o arquivo
+        System.out.println("getPath: "+path.getPath());
+
+        sc.close();
+    }
+}
