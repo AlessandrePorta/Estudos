124fca35b17b88db4825c608e63be4b00cb807de
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-10 17:20:24 -0300

    ExercicioResolvido1

---
+++ src/Interfaces/Enum.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+public enum Enum {
+
+    PAGAMENTO_PENDENTE,
+    PROCESSANDO,
+    ENVIADO,
+    ENTREGUE,
+}
---
+++ src/Interfaces/ExercicioExemplo1.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public class ExercicioExemplo1 {
+    public static void main(String[] args) {
+
+    }
+}
