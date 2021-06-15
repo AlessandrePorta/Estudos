961bb384e2774679419f19c4dbc84e7c6a5f7188
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-15 14:57:57 -0300

    Principios get e put com uma lista generica

---
+++ src/Generics/PrincipiosGetEPut/Application.java
@@ -0,0 +1,29 @@
+package Generics.PrincipiosGetEPut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(1,2,3,4);
+        List<Double> myDoubles = Arrays.asList(3.14,6.28);
+        List<Object> myObjs = new ArrayList<>();
+
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
+    public static void copy(List<? extends Number> source, List<? super Number> destiny){
+        for(Number number : source){
+            destiny.add(number);
+        }
+    }
+    public static void printList(List<?> lista){
+        for(Object o : lista){
+            System.out.println(o + " ");
+        }
+        System.out.println();
+    }
+}
