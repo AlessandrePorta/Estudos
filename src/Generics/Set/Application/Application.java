3f34fe09b042e9d43ceb8d5ddbffcbbdbb00b2de
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-17 19:03:10 -0300

    Set

---
+++ src/Generics/Set/Application/Application.java
@@ -0,0 +1,32 @@
+package Generics.Set.Application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Application {
+    public static void main(String[] args) {
+
+        //HashSet mais rapido porém sem ordem
+        //TreeSet mais demorado porém ordenado
+        //LinkedHashSet mantém a ordem do que foi adicionado
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+
+        set.remove("Notebook");
+
+        //Remove ou adiciona de acordo com o predicado
+        set.removeIf(x -> x.length() >= 3);
+        //Remove de acordo com o char descrito sobre ' '
+        set.removeIf(x -> x.charAt(0) == 'N');
+
+        System.out.println(set.contains("Notebook"));
+
+        for(String p : set){
+            System.out.println(p);
+        }
+    }
+}
---
+++ src/Generics/Set/Application/Application1.java
@@ -0,0 +1,30 @@
+package Generics.Set.Application;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+
+        //Unir as 2 listas
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+
+        //Apenas os elementos em comum em uma lista
+
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+
+        //Remover todos os itens de uma lista que são iguais a outra
+
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
+    }
+}
