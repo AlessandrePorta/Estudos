2907069e8853977192479a4e741fc9b581791dce
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-24 16:56:03 -0300

    Exercicio MAP finalizado

--- src/Exercicios/ExercicioFixacaoCap19/Map/Application/Application.java
+++ src/Exercicios/ExercicioFixacaoCap19/Map/Application/Application.java
@@ -1,10 +1,7 @@
 package Exercicios.ExercicioFixacaoCap19.Map.Application;
 
-import Exercicios.ExercicioFixacaoCap19.Map.Entities.Candidate;
-
 import java.io.*;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,30 +9,34 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.US);
 
+        Map<String, Integer> candidate = new LinkedHashMap<>();
+
         File path = new File("C:\\Users\\alessandrejunior-sgk\\Desktop\\in.csv");
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-            for (int i = 0; i < 4; i++) {
-                String[] str1 = sc.nextLine().split(", ");
-                Candidate p = new Candidate(str1[0], Integer.parseInt(str1[1]));
-                bw.write(p.getName() + "; " + p.getVotes());
-                bw.newLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String str = br.readLine();
+            while (str != null) {
+                String[] fields = str.split(",");
+                String name = fields[0];
+                Integer votes = Integer.parseInt(fields[1]);
+
+                if(candidate.containsKey(name)){
+                    int soma = candidate.get(name);
+                    candidate.put(name, votes + soma);
+                }
+                else {
+                    candidate.put(name, votes);
+                }
+                str = br.readLine();
+
+            }
+            System.out.println("VOTACAO:");
+            for(String key : candidate.keySet()){
+                System.out.println(key + ": "+ candidate.get(key));
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String str1 = br.readLine();
-            while (str1 != null) {
-                String[] str = str1.split("; ");
-                Candidate p = new Candidate(str[0], Integer.parseInt(str[1]));
-                System.out.println(p.getName() + ", " + p.getVotes());
-                str1 = br.readLine();
-            }
-
-        } catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
-
     }
 }
